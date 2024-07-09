ce3aa319a81e61576e818c10173d923663b61f9d
Praveen <baira_praveen@optum.com>
2024-07-09 17:14:51 +0530

    feat: initial commit

---
+++ src/Components/Main/Home.jsx
@@ -0,0 +1,7 @@
+import { Header } from "../layout/Header"
+
+export const Home = () => {
+    return (
+        <Header />
+    )
+}
---
+++ src/Pages/homePage.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react"
+import { Home } from "../Components/Main/Home"
+
+export const HomePage = () => {
+    return (
+        <Fragment>
+            <Home />
+        </Fragment>
+    )
+}
