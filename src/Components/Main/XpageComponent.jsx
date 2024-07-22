951fde40048a518350fef473567e37298b6999bc
Praveen@optum <baira_praveen@optum.com>
2024-07-22 20:29:41 +0530

    feat: page 3 mostly over

--- src/Components/Main/XpageComponent.jsx
+++ src/Components/Main/XpageComponent.jsx
@@ -1,5 +1,5 @@
-import { Circle, Delete, Download, DownloadForOffline } from "@mui/icons-material";
-import { Box, CircularProgress, Grid, InputLabel, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Menu, DownloadForOffline } from "@mui/icons-material";
+import { Box, Checkbox, CircularProgress, FormControlLabel, FormGroup, Grid, InputLabel, List, ListItem, ListItemText, Tooltip, Typography, styled, tooltipClasses } from "@mui/material";
 import PropTypes from 'prop-types';
 import { Fragment } from "react";
 import classes from "./XpageComponent.module.css";
@@ -7,13 +7,41 @@ import classes from "./XpageComponent.module.css";
 const LabelAnswerComponent = ({ label, subText, bigGrid }) => {
   return (
     <Fragment>
-      <Typography component="p" color={bigGrid ? 'secondary' : '#020202'}>{label}</Typography>
-      <Typography component="p" color={bigGrid ? 'secondary' : 'primary'}>
+      <Typography component="p" color={bigGrid ? 'secondary' : '#020202'} sx={bigGrid && { fontWeight: '600' }}>{label}</Typography>
+      <Typography component="p" color={bigGrid ? 'secondary' : 'primary'} sx={bigGrid && { fontWeight: '600' }}>
         {subText}
       </Typography>
     </Fragment>
   );
 };
+
+const MenuTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#ffffff',
+    color: '#8e8e8e',
+    boxShadow: 'rgba(0, 0, 0, 0.25) 0px 25px 50px -12px',
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: '#ffffff',
+    boxShadow: 'rgba(0, 0, 0, 0.25) 0px 25px 50px -12px',
+  }
+}));
+
+const CustomFormControlLabel = styled(FormControlLabel)(({ theme }) => ({
+  '& .MuiFormControlLabel-label': {
+    color: '#c0c0c0'
+  },
+  '& .MuiCheckbox-root.Mui-checked': {
+    color: '#02a91f',
+  },
+  '& .MuiCheckbox-root.Mui-checked + .MuiFormControlLabel-label': {
+    color: '#02a91f',
+    fontWeight: '700'
+  }
+}
+))
 
 function CircularProgressWithLabel(props) {
   return (
@@ -48,20 +76,36 @@ CircularProgressWithLabel.propTypes = {
   value: PropTypes.number.isRequired,
 };
 
+const MenuTooltipFragment = () => {
+  return (
+    <div className={classes.menuTooltipFragment}>
+      <LabelAnswerComponent label="Request Number" subText="0123456" />
+      <LabelAnswerComponent label="valiated By" subText="Rohit" />
+    </div>
+  )
+}
+
 const NestedGridItem = () => {
   return (
     <div className={classes.nestedGridItem}>
       <div className={classes.nestedGridItemOne}>
-        <LabelAnswerComponent label="SECTION" subText="Contract Term" />
+        <Typography component="p" color="primary" sx={{ fontWeight: 'bold' }}>Contract Term</Typography>
+        <MenuTooltip title={<MenuTooltipFragment />} placement="right-end" arrow>
+
+          <Menu />
+
+        </MenuTooltip>
       </div>
       <div className={classes.nestedGridItemTwo}>
-        <DownloadForOffline />
-      </div>
-      <div className={classes.nestedGridItemThree}>
-        <LabelAnswerComponent label="STATUS" subText="Digitized" />
-      </div>
-      <div className={classes.nestedGridItemFour}>
-        <LabelAnswerComponent label="AUDIT VERSION" subText="0013" />
+        <Typography component="p" color="primary" sx={{ fontWeight: 'bold' }}>Processed</Typography>
+        <FormGroup>
+          <CustomFormControlLabel
+            color="success"
+            control={<Checkbox size="small" />}
+            label="Audit"
+            sx={{ margin: 0 }}
+          />
+        </FormGroup>
       </div>
     </div>
   );
@@ -104,11 +148,11 @@ export const XpageComponent = () => {
             <div className={classes.gridItemOneInsideOne}>
               <LabelAnswerComponent label="Client Name" subText="Eternal Health Corporation" bigGrid={true} />
             </div>
-            <div>
+            <div className={classes.gridItemOneInsideTwo}>
               <DownloadForOffline />
             </div>
-            <div>
-              <LabelAnswerComponent label="File Name" subText="123456.PDF" bigGrid={true} />
+            <div className={classes.gridItemOneInsideThree}>
+              <LabelAnswerComponent label="File Name" subText="Brand File Name sample_123456.PDF" bigGrid={true} />
             </div>
             <div>
               <LabelAnswerComponent label="Agreement Number" subText="985674839301123" bigGrid={true} />
@@ -118,6 +162,9 @@ export const XpageComponent = () => {
             </div>
             <div>
               <LabelAnswerComponent label="Version ID" subText="030" bigGrid={true} />
+            </div>
+            <div>
+              <LabelAnswerComponent label="Audit Version" subText="#01" bigGrid={true} />
             </div>
           </div>
         </div>
@@ -140,14 +187,14 @@ export const XpageComponent = () => {
           <NestedGridItem />
         </div>
       </div>
-      <div className={classes.secondPart}>
+      {/* <div className={classes.secondPart}>
         <DownGridItem />
         <DownGridItem />
       </div>
       <div className={classes.lastRow}>
         <Typography component="p" sx={{mx: 2, color: 'orange'}}><Circle sx={{ fontSize: '0.5rem'}} /> Custom</Typography>
         <Typography component="p" sx={{mx: 2, color: 'green'}}><Circle sx={{ fontSize: '0.5rem'}} /> Medispan</Typography>
-      </div>
+      </div> */}
     </div>
   );
 };
